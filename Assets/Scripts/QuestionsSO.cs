f9acca47b569bf0733184e9b66a5d9c5a38147f4
steven stearns <stearn102@morrisville.edu>
2022-03-08 14:40:23 -0500

    Arrays

--- Assets/Scripts/QuestionsSO.cs
+++ Assets/Scripts/QuestionsSO.cs
@@ -7,11 +7,23 @@ public class QuestionsSO : ScriptableObject
 {
     [TextArea(2,6)] 
     [SerializeField] string strQuestion = "Enter new question text here";
+    [SerializeField] string[] stranswers = new string[4];
+    [SerializeField] int intCorrectAnswerIndex;
 
     public string GetQuestion()
     {
         return strQuestion;
     }
-}
+    
+    public int GetAnswerIndex()
+    {
+        return intCorrectAnswerIndex;
+    }
+
+    public string GetAnswer(int index)
+    {
+        return stranswers[index];
+    }
+} // QuestionSO
 
 
